88fc61fe1b481e3aac43e979e2d418cc1255385c
aranparrk <aranparrk@gmail.com>
2021-12-28 21:23:15 +0900

    [Update] ItemView와 UserView 라우터 구현

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import VueRouter from 'vue-router';
 import NewsView from '../views/NewsView.vue';
 import AskView from '../views/AskView.vue';
 import JobsView from '../views/JobsView.vue';
+import UserView from '../views/UserView.vue';
+import ItemView from '../views/ItemView.vue';
 
 // # 2 Vue.use로 VueRouter 추가
 Vue.use(VueRouter);
@@ -27,6 +29,14 @@ export const router = new VueRouter({
     {
         path: '/jobs',
         component: JobsView
-    }    
+    },
+    {
+        path: '/user',
+        component: UserView
+    },
+    {
+        path: '/item',
+        component: ItemView
+    }
   ]
 });
