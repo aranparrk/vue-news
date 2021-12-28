f2cffd43e7b00eee05be688956600b6676d301f7
aranparrk <aranparrk@gmail.com>
2021-12-28 17:38:54 +0900

    [Update] 라우터와 컴포넌트 연결

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 // # 1 라우터 설치 후 main.js에서 import
 import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.Vue';
 
 // # 2 Vue.use로 VueRouter 추가
 Vue.use(VueRouter);
@@ -8,17 +11,17 @@ const router = new VueRouter({
   routes: [
     {
         // path: url 주소
-        path: '',
+        path: '/news',
         // component: url 주소로 갔을 때 표시 될 컴포넌트
-        component: ''
+        component: NewsView
     },
     {
-        path: '',
-        component: ''
+        path: '/ask',
+        component: AskView
     },
     {
-        path: '',
-        component: ''
+        path: '/jobs',
+        component: JobsView
     }    
   ]
 });
