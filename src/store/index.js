d62a47461fd6d6abd6da4d77ff20d5aa461b4d95
aranparrk <aranparrk@gmail.com>
2021-12-29 15:11:34 +0900

    [Update] NewsView에 actions와 mutations 적용

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { fetchNewsList } from '../api/index.js';
 
 Vue.use(Vuex);
 
@@ -7,5 +8,25 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
     state: {
         news: []
+    },
+    // # 1 vuex를 이용해서 api 호출, 비동기 호출
+    actions: {
+        FETCH_NEWS(context) {
+            fetchNewsList()
+            .then(response => {
+                console.log(response.data);
+                // # 3 mutations 호출
+                context.commit('SET_NEWS', response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
+    },
+    // # 2 actions에서 호출한 api를 state에 담기 위한 절차
+    mutations: {
+        SET_NEWS(state, news) {
+            state.news = news;
+        }
     }
 })
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,33 +1,27 @@
 <template>
     <div>
-        <div v-for="user in users">{{ user.title }}</div>
+        <div v-for="user in this.$store.state.news">{{ user.title }}</div>
     </div>
 </template>
 
 <script>
-import { fetchNewsList } from '../api/index.js';
-
 export default {
-    data() {
-        return {
-            users: []
-        }
-    },
     created() {
-        console.log('호출 전 : ', this);
+        this.$store.dispatch('FETCH_NEWS');
+    //     console.log('호출 전 : ', this);
 
-        // # 1 데이터 호출
-        fetchNewsList()
-        .then(response => {
-            console.log('NewsView Data : ' + response);
-            // 화살표 함수로 바인딩을 했을 경우
-            console.log('호출 후 : ', this);
-            // # 2 데이터 담기
-            this.users = response.data;
-        })
-        .catch(function(error){
-            console.log(error);
-        })
+    //     // # 1 데이터 호출
+    //     fetchNewsList()
+    //     .then(response => {
+    //         console.log('NewsView Data : ' + response);
+    //         // 화살표 함수로 바인딩을 했을 경우
+    //         console.log('호출 후 : ', this);
+    //         // # 2 데이터 담기
+    //         this.users = response.data;
+    //     })
+    //     .catch(function(error){
+    //         console.log(error);
+    //     })
     }
 }
 </script>
