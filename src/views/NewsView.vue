ca3b9f7fa81c74f6c6ebffc78b446b0597489d75
aranparrk <aranparrk@gmail.com>
2021-12-29 15:04:03 +0900

    [Update] api 호출 방식

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -15,18 +15,20 @@ export default {
     },
     created() {
         console.log('호출 전 : ', this);
+
+        // # 1 데이터 호출
         fetchNewsList()
         .then(response => {
             console.log('NewsView Data : ' + response);
             // 화살표 함수로 바인딩을 했을 경우
             console.log('호출 후 : ', this);
+            // # 2 데이터 담기
             this.users = response.data;
         })
         .catch(function(error){
             console.log(error);
         })
     }
-
 }
 </script>
 
