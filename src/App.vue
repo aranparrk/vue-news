11e3968bd2aac6a8c2f2ee7db6cec3fa284c0d6f
aranparrk <aranparrk@gmail.com>
2021-12-30 15:20:46 +0900

    [Update] 라우터 트랜지션 구현

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
     <!-- 2 - Essential -->
     <!-- 플러그인의 자동완성 / VS 코드가 제공하는 기능 -->
     <tool-bar></tool-bar>
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -23,5 +25,14 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+
+/* Router Transition */
+.page-enter-active, .page-leave-active {
+  transition: opacity .5s;
+}
+
+.page-enter, .page-leave-to /* .page-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
