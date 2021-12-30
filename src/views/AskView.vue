3dab3f3a5f4a24531b8a62de34a0518a50417e63
aranparrk <aranparrk@gmail.com>
2021-12-30 18:43:09 +0900

    [Update] 공통 컴포넌트 구현(1) - 페이지별 데이터 분기

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,63 +1,16 @@
 <template>
-    <div>
-        <ul class="ask-list">
-            <li v-for="ask in this.$store.state.ask" class="ask">
-                <div class="points">
-                    {{ ask.points }}
-                </div>
-                <div>
-                    <p class="ask-title">
-                        <a v-bind:href="ask.url">
-                                <router-link v-bind:to="`/item/${ask.id}`">
-                                    {{ ask.title }}>
-                                </router-link>
-                        </a>
-                    </p>
-                    <small class="link-text">
-                        {{ ask.time_ago }} by 
-                        <router-link v-bind:to="`/user/${ask.user}`" class="link-text">{{ ask.user }}</router-link>
-                    </small>
-                </div>
-            </li>
-        </ul>
-    </div>
+    <ask-view></ask-view>
 </template>
 
 <script>
+import AskView from '../components/ListItem.vue'
 export default {
-    created() {
-        this.$store.dispatch('FETCH_ASK');
+    components: {
+        AskView
     }
 }
 </script>
 
 <style>
-.ask-list {
-    margin: 0;
-    padding: 0;
-}
 
-.ask {
-    list-style: noen;
-    display: flex;
-    align-items: center;
-    border-bottom: 1px solid #eee;
-}
-
-.points {
-    width: 80px;
-    height: 60px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: #42b883;
-}
-
-.ask-title {
-    margin: 0;
-}
-
-.link-text {
-    color: #828288;
-}
 </style>
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,78 +1,16 @@
 <template>
-    <div>
-        <ul class="new-list">
-            <li v-for="item in this.$store.state.news" class="post">
-                <!-- 포인트 영역 -->
-                <div class="points">
-                    {{ item.points }}
-                </div>
-                <!-- 기타 정보 영역 -->
-                <div>
-                    <p class="news-title">
-                        <a v-bind:href="item.url">
-                            {{ item.title }}
-                        </a>
-                    </p>
-                    <small class="link-text">
-                        {{ item.time_ago }} by
-                        <!-- <router-link v-bind:to="'/user/' + item.user">{{ item.user }}</router-link> -->
-                        <router-link v-bind:to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
-                    </small>
-                </div>
-            </li>
-        </ul>
-    </div>
+    <news-view></news-view>
 </template>
 
 <script>
+import NewsView from '../components/ListItem.vue'
 export default {
-    created() {
-        this.$store.dispatch('FETCH_NEWS');
-    //     console.log('호출 전 : ', this);
-
-    //     // # 1 데이터 호출
-    //     fetchNewsList()
-    //     .then(response => {
-    //         console.log('NewsView Data : ' + response);
-    //         // 화살표 함수로 바인딩을 했을 경우
-    //         console.log('호출 후 : ', this);
-    //         // # 2 데이터 담기
-    //         this.users = response.data;
-    //     })
-    //     .catch(function(error){
-    //         console.log(error);
-    //     })
+    components: {
+        NewsView
     }
 }
 </script>
 
-<style scoped>
-.new-list {
-    margin: 0;
-    padding: 0;
-}
+<style>
 
-.post {
-    list-style: noen;
-    display: flex;
-    align-items: center;
-    border-bottom: 1px solid #eee;
-}
-
-.points {
-    width: 80px;
-    height: 60px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: #42b883;
-}
-
-.news-title {
-    margin: 0;
-}
-
-.link-text {
-    color: #828288;
-}
 </style>
